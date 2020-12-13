1ac9bfba7793380aae41817fb189884a330a84dd
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2020-12-13 00:30:56 -0500

    Format arduino_debug.h

--- src/arduino_debug.h
+++ src/arduino_debug.h
@@ -10,26 +10,27 @@
 
 #define DEBUG_OUTPUT_MAX_SIZE 100
 
-#define debug_serial_printf(message, ...)                                     \
-  do {                                                                        \
-    if (DEBUG_TEST) {                                                         \
-      Serial.printf("%s line:%d: " message, __func__, __LINE__, __VA_ARGS__); \
-    }                                                                         \
-  } while (0)
+#define debug_serial_printf(message, ...)                                      \
+    do {                                                                       \
+        if (DEBUG_TEST) {                                                      \
+            Serial.printf("%s line:%d: " message, __func__, __LINE__,          \
+                          __VA_ARGS__);                                        \
+        }                                                                      \
+    } while (0)
 
-#define debug_serial_printfln(message, ...) \
-  debug_serial_printf(message, __VA_ARGS__)
+#define debug_serial_printfln(message, ...)                                    \
+    debug_serial_printf(message, __VA_ARGS__)
 
-#define debug_serial_println(message) \
-  do {                                \
-    if (DEBUG_TEST) {                 \
-      Serial.println(message);        \
-    }                                 \
-  } while (0)
+#define debug_serial_println(message)                                          \
+    do {                                                                       \
+        if (DEBUG_TEST) {                                                      \
+            Serial.println(message);                                           \
+        }                                                                      \
+    } while (0)
 
-#define debug_serial_print(message) \
-  do {                              \
-    if (DEBUG_TEST) {               \
-      Serial.print(message);        \
-    }                               \
-  } while (0)
+#define debug_serial_print(message)                                            \
+    do {                                                                       \
+        if (DEBUG_TEST) {                                                      \
+            Serial.print(message);                                             \
+        }                                                                      \
+    } while (0)
